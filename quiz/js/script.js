ec34e211ae65b2e6b86a5782e48be3eaf31e1fc0
Fajarshodiq1 <fajarshodiq1220@gmail.com>
2023-12-19 00:04:25 +0700

    update fitur klaim sertifikat

--- quiz/js/script.js
+++ quiz/js/script.js
@@ -171,27 +171,27 @@ function showResult() {
     // if user scored more than 3
     //creating a new span tag and passing the user score number and total question number
     let scoreTag =
-      "<span>and congrats! 🎉, You got <p>" +
+      "<span>🎉, Kamu Mendapatkan <p>" +
       userScore +
-      "</p> out of <p>" +
+      "</p> dari <p>" +
       questions.length +
       "</p></span>";
     scoreText.innerHTML = scoreTag; //adding new span tag inside score_Text
   } else if (userScore > 1) {
     // if user scored more than 1
     let scoreTag =
-      "<span>and nice 😎, You got <p>" +
+      "<span>😎, Kamu Mendapatkan <p>" +
       userScore +
-      "</p> out of <p>" +
+      "</p> dari <p>" +
       questions.length +
       "</p></span>";
     scoreText.innerHTML = scoreTag;
   } else {
     // if user scored less than 1
     let scoreTag =
-      "<span>and sorry 😐, You got only <p>" +
+      "<span>😐, Kamu Mendapatkan <p>" +
       userScore +
-      "</p> out of <p>" +
+      "</p> dari <p>" +
       questions.length +
       "</p></span>";
     scoreText.innerHTML = scoreTag;
